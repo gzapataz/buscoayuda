10fad0b90d9e0893c8d0066153d48f41e617b9ed
Gabriel Zapata <gab.zapata@uniandes.edu.co>
2017-04-02 17:06:51 -0500

    Paso 2 - Test para probar registro de un independientel (Rojo)

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,18 @@
-from django.conf import settings
 from django.conf.urls import url
-from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ir_index, name=''),
+    url(r'^$', views.index, name='index'),
+    url(r'^editar/(?P<idTrabajador>\d+)$', views.editar_perfil, name='editar'),
+    url(r'^login$', views.login),
+    url(r'^register', views.register),
+    url(r'^logout$', views.logout),
+    url(r'^trabajador/(?P<pk>\d+)$', views.detail),
+    url(r'^detail', views.detalle_trabajador),
+    url(r'^addComment', views.add_comment),
+    url(r'^mostrarComentarios/(?P<idTrabajador>\d+)$', views.mostrarComentarios),
+    url(r'^mostrarTrabajadores/(?P<tipo>\w+)$', views.mostrarTrabajadores),
+    url(r'^mostrarTrabajadores', views.mostrarTrabajadores),
+    url(r'^tipoServicio/(?P<pk>\d+)$', views.getTiposDeServicio),
 ]
