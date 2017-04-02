dbc4ab9c5759641049d6a251ff811d7ce1836e09
Gabriel Zapata <gab.zapata@uniandes.edu.co>
2017-04-02 16:21:31 -0500

    First commit

---
+++ polls/Functional_test.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from selenium import webdriver
+
+class FunctionalTest(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Safari()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Busco Ayuda', self.browser.title)
